ce644654477a2cdd435ea178cede02f1c4f31ec4
wuwei <wuweihku@163.com>
2015-12-28 17:38:03 +0800

    finishing log

--- Passport_Interface_Testing.py
+++ Passport_Interface_Testing.py
@@ -1,37 +1,40 @@
 # -*- coding: utf-8 -*-
-import unittest                                                                         #支持Python单元测试模块
-import urllib.parse                                                                     #这里urllib.parse要精确到子模块,否则会报错
-import urllib.request                                                                   #这里urllib.request要精确到子模块,否则会报错
-import csv                                                                              #支持csv自动化导表
+import unittest                                                                             #支持Python单元测试模块
+import urllib.parse                                                                         #这里urllib.parse要精确到子模块,否则会报错
+import urllib.request                                                                       #这里urllib.request要精确到子模块,否则会报错
+import csv                                                                                  #支持csv自动化导表
+import sys                                                                                  #用来打log
+
 
 '''
-一个子功能模块对应一个测试类
+一个子功能模块对应一个测试类,此版本将输出信息存储于log文件夹下的log文件之中，再将当次的log文件print到屏幕上，方便查看信息
 '''
 
 #对应模块:2.1用户名注册
 class test_signup(unittest.TestCase):
-    url = 'http://passportapi.15166.com/user/signup'                                    #所要访问的url,一个测试类对应一个url
+    url = 'http://passportapi.15166.com/user/signup'                                        #所要访问的url,一个测试类对应一个url
     
-    def setUp(self):                                                                    #文件/数据库/网络服务初始化工作
+    def setUp(self):                                                                        #文件/数据库/网络服务初始化工作
         pass
 
-    def tearDown(self):                                                                 #销毁工作
+    def tearDown(self):                                                                     #销毁工作
         pass
 
-    def test_signup_cases(self):                                                      #执行测试功能的函数
-        with open('signup_data.csv') as csvfile:                                      #打开csv文件流
-            reader = csv.DictReader(csvfile)                                            #创建文件流对象
-            signup_num=0                                                              #计算一共跑了多少条测试数据
-            for row in reader:                                                          #这里的row对应csv表里的一行数据,第一行数据自动作为字段名,第二行数据开始作为测试实例
+    def test_signup_cases(self):                                                            #执行测试功能的函数
+        with open('signup_data.csv') as csvfile:                                            #打开csv文件流
+            reader = csv.DictReader(csvfile)                                                #创建文件流对象
+            signup_num=0                                                                    #计算一共跑了多少条测试数据
+            for row in reader:                                                              #这里的row对应csv表里的一行数据,第一行数据自动作为字段名,第二行数据开始作为测试实例
                 signup_num+=1                                                         
-                with self.subTest(row=row):                                             #row=i,会报错row is not defined,必须用row=row(这里用的是subTest功能)
+                with self.subTest(row=row):                                                 #row=i,会报错row is not defined,必须用row=row(这里用的是subTest功能)
                     print("正在为'用户名注册模块'执行第 %d 条测试数据"%signup_num)                                              #每跑一条数据,显示一次当前进度
                     info = {'appid': row['appid'], 'username': row['username'], 'password': row['password'], 'repassword': row['repassword']} #csv里的每一行测试实例，这里不用过滤空值，空值可以作为测试用例，引发异常
-                    data = urllib.parse.urlencode(info).encode(encoding='UTF8')         #将信息编码成urllib能够识别的类型,注意的是python2.7用的ASCII编码,python3.X要UTF8转码  
-                    req = urllib.request.Request(test_signup.url, data)               #构造请求对象  
-                    response = urllib.request.urlopen(req)                              #执行post请求
-                    response_dict = eval(response.read())                               #读取发回的数据,并将字符串转换为字典 
-                    self.assertEqual(response_dict["res"], eval(row['res']))            #用户名注册模块---您看到此信息,代表当行测试数据未通过---   
+                    data = urllib.parse.urlencode(info).encode(encoding='UTF8')             #将信息编码成urllib能够识别的类型,注意的是python2.7用的ASCII编码,python3.X要UTF8转码  
+                    req = urllib.request.Request(test_signup.url, data)                     #构造请求对象  
+                    response = urllib.request.urlopen(req)                                  #执行post请求
+                    response_dict = eval(response.read())                                   #读取发回的数据,并将字符串转换为字典 
+                    self.assertEqual(response_dict["res"], eval(row['res']))    #用户名注册模块---您看到此信息,代表当行测试数据未通过---  
+                 
         print("----------------------------------------------------------------------------------------------------------------------")
 #对应模块:2.2登录及快速登录
 class test_login(unittest.TestCase):
@@ -60,18 +63,18 @@ class test_login(unittest.TestCase):
                     #所以在这里读取字典的时候，就要过滤空值了
                 
                     info = {'appid': row['appid'], 'username': row['username'], 'mail': row['mail'], 'phone': row['phone'],'password': row['password'],'guid': row['guid'],'sessionid': row['sessionid']}
-                    list_del = []                                                       #定义一个用来存储空值键的列表
+                    list_del = []                                                           #定义一个用来存储空值键的列表
                     for k in info.keys():
-                        if info[k] =='':                                                #判断对应键的值为空时，将该键加入list_del待删除
+                        if info[k] =='':                                                    #判断对应键的值为空时，将该键加入list_del待删除
                             list_del.append(k)
-                    for i in list_del:                                                  #一一删除存储在list_del中的空值键
+                    for i in list_del:                                                      #一一删除存储在list_del中的空值键
                         del info[i] 
 
                     data = urllib.parse.urlencode(info).encode(encoding='UTF8')
                     req = urllib.request.Request(test_login.url, data) 
                     response = urllib.request.urlopen(req) 
                     response_dict = eval(response.read())
-                    self.assertEqual(response_dict["res"], eval(row['res']))            #登录、快速登录模块---您看到此信息,代表当行测试数据未通过---    
+                    self.assertEqual(response_dict["res"], eval(row['res']))    #登录、快速登录模块---您看到此信息,代表当行测试数据未通过---    
         print("----------------------------------------------------------------------------------------------------------------------")
 
 #对应模块:2.3获取绑定手机
@@ -98,7 +101,7 @@ class test_findbindphone(unittest.TestCase):
                     req = urllib.request.Request(test_findbindphone.url, data)
                     response = urllib.request.urlopen(req)
                     response_dict = eval(response.read())
-                    self.assertEqual(response_dict["res"], eval(row['res']))            #获取绑定手机模块---您看到此信息,代表当行测试数据未通过---                        
+                    self.assertEqual(response_dict["res"], eval(row['res']))    #获取绑定手机模块---您看到此信息,代表当行测试数据未通过---                        
         print("----------------------------------------------------------------------------------------------------------------------")
 
 #对应模块:2.4获取验证码
@@ -131,7 +134,7 @@ class test_getcode(unittest.TestCase):
                     # 下面的代码用于显示一个5分钟有效期的code_id，以方便2.5/2.6模块
                     # print(response_dict["code_id"]+"用于显示一个5分钟有效期的code_id,以方便2.5/2.6模块进行测试")
                     # print(response_dict)
-                    self.assertEqual(response_dict["res"], eval(row['res']))            #获取验证码模块---您看到此信息,代表当行测试数据未通过---
+                    self.assertEqual(response_dict["res"], eval(row['res']))    #获取验证码模块---您看到此信息,代表当行测试数据未通过---
         print("----------------------------------------------------------------------------------------------------------------------")
                     
 #对应模块:2.5找回密码
@@ -164,7 +167,7 @@ class test_changepwdbycode(unittest.TestCase):
                     req = urllib.request.Request(test_changepwdbycode.url, data)  
                     response = urllib.request.urlopen(req)
                     response_dict = eval(response.read())
-                    self.assertEqual(response_dict["res"], eval(row['res']))            #找回密码模块---您看到此信息,代表当行测试数据未通过---
+                    self.assertEqual(response_dict["res"], eval(row['res']))    #找回密码模块---您看到此信息,代表当行测试数据未通过---
         print("----------------------------------------------------------------------------------------------------------------------")
             
 #对应模块:2.6绑定手机
@@ -192,7 +195,7 @@ class test_bindphone(unittest.TestCase):
                     req = urllib.request.Request(test_bindphone.url, data)
                     response = urllib.request.urlopen(req)
                     response_dict = eval(response.read())
-                    self.assertEqual(response_dict["res"], eval(row['res']))            #绑定手机模块---您看到此信息,代表当行测试数据未通过---
+                    self.assertEqual(response_dict["res"], eval(row['res']))    #绑定手机模块---您看到此信息,代表当行测试数据未通过---
         print("----------------------------------------------------------------------------------------------------------------------")
 
 #对应模块:2.7修改信息
@@ -219,7 +222,7 @@ class test_editinfo(unittest.TestCase):
                     req = urllib.request.Request(test_editinfo.url, data)
                     response = urllib.request.urlopen(req)
                     response_dict = eval(response.read())
-                    self.assertEqual(response_dict["res"], eval(row['res']))            #修改信息模块---您看到此信息,代表当行测试数据未通过---
+                    self.assertEqual(response_dict["res"], eval(row['res']))    #修改信息模块---您看到此信息,代表当行测试数据未通过---
         print("----------------------------------------------------------------------------------------------------------------------")
 
 #对应模块:2.8完善用户名
@@ -245,12 +248,18 @@ class test_addusername(unittest.TestCase):
                     req = urllib.request.Request(test_addusername.url, data)
                     response = urllib.request.urlopen(req)
                     response_dict = eval(response.read())
-                    self.assertEqual(response_dict["res"], eval(row['res']))            #完善用户名模块---您看到此信息,代表当行测试数据未通过---
+                    self.assertEqual(response_dict["res"], eval(row['res']))    #完善用户名模块---您看到此信息,代表当行测试数据未通过---
         print("----------------------------------------------------------------------------------------------------------------------")
                     
                  
 '''
 unittest.main(),固定格式,用于默认调用unittest模块
 '''
+import time
+
 if __name__ == '__main__':
-    unittest.main()
+    log_file = 'log/log_%s.txt'%time.strftime("%Y_%m_%d_%H:%M:%S", time.localtime()) #定义log路径及文件名
+    f = open(log_file, "w")
+    runner = unittest.TextTestRunner(f)
+    unittest.main(testRunner=runner)
+    f.close()
